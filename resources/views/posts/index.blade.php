5622df18ef268f1705776552a5d58c093b2d2d49
1210osky <kyo.04150415@outlook.jp>
2022-01-10 16:18:49 +0900

    記事作成画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
+        [<a href='/posts/create'>create</a>]
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <div class='post'>
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
